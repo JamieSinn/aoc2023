0fa21c040b5fe5aae7ea97b8508bf737cef11e6a
Jamie Sinn <jamie.sinn@taplytics.com>
2023-12-01 13:15:36 -0800

    Working on part 2

--- 01/day1.go
+++ 01/day1.go
@@ -3,6 +3,7 @@ package day1
 import (
 	"io"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -26,21 +27,76 @@ func Day1(fileName string) {
 }
 
 func evaluateLine(line string) int {
+	regexPattern := "(one|two|three|four|five|six|seven|eight|nine)"
+	numberPositions := map[int]int{0: -1, 1: -1}
 	firstNumber := 0
 	lastNumber := 0
 	firstFound := false
 	sum := 0
+	wordMatcher, err := regexp.Compile(regexPattern)
+	if err != nil {
+		panic(err)
+	}
 
-	for _, c := range line {
+	for i, c := range line {
 		if d, err := strconv.ParseInt(string(c), 10, 64); err == nil {
 			if !firstFound {
 				firstNumber = int(d)
 				lastNumber = firstNumber
 				firstFound = true
+				numberPositions[0] = i
+				numberPositions[1] = i
 			} else {
 				lastNumber = int(d)
+				numberPositions[1] = i
 			}
 		}
+	}
+
+	wordMatches := wordMatcher.FindAllString(line, -1)
+	workingLine := line
+	for _, match := range wordMatches {
+		n := -1
+		switch match {
+		case "one":
+			n = 1
+			break
+		case "two":
+			n = 2
+			break
+		case "three":
+			n = 3
+			break
+		case "four":
+			n = 4
+			break
+		case "five":
+			n = 5
+			break
+		case "six":
+			n = 6
+			break
+		case "seven":
+			n = 7
+			break
+		case "eight":
+			n = 8
+			break
+		case "nine":
+			n = 9
+			break
+		default:
+			panic("unknown word")
+		}
+		index := strings.Index(workingLine, match)
+		if numberPositions[0] < 0 || index < numberPositions[0] {
+			firstNumber = n
+			numberPositions[0] = index
+		} else if numberPositions[1] < 0 || index > numberPositions[1] {
+			lastNumber = n
+			numberPositions[1] = index
+		}
+		workingLine = strings.Replace(workingLine, match, strings.Repeat("_", len(match)), 1)
 	}
 	sum += firstNumber*10 + lastNumber
 	return sum
--- 01/day1_test.go
+++ 01/day1_test.go
@@ -7,10 +7,11 @@ import (
 	"testing"
 )
 
-var lineValues = map[int]int{0: 12, 1: 38, 2: 15, 3: 77}
+var calibration1 = map[int]int{0: 12, 1: 38, 2: 15, 3: 77}
+var calibration2 = map[int]int{0: 29, 1: 83, 2: 13, 3: 24, 4: 42, 5: 14, 6: 76}
 
 func TestDay1(t *testing.T) {
-	file, err := os.OpenFile("calib.txt", os.O_RDONLY, 0644)
+	file, err := os.OpenFile("calib1.txt", os.O_RDONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
@@ -23,9 +24,28 @@ func TestDay1(t *testing.T) {
 
 	for i, line := range lines {
 		lineVal := evaluateLine(line)
-		if lineValues[i] != lineVal {
-			t.Errorf("index: %d did not equal expected - %d - actual %d", i, lineValues[i], lineVal)
+		if calibration1[i] != lineVal {
+			t.Errorf("index: %d did not equal expected - %d - actual %d", i, calibration1[i], lineVal)
 		}
 	}
+}
 
+func TestDay1_Words(t *testing.T) {
+	file, err := os.OpenFile("calib2.txt", os.O_RDONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	for i, line := range lines {
+		lineVal := evaluateLine(line)
+		if calibration2[i] != lineVal {
+			t.Errorf("index: %d did not equal expected - %d - actual %d", i, calibration2[i], lineVal)
+		}
+	}
 }
